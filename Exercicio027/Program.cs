d22a30442b69c0fe0593d77b587f677520f9df3f
Charles Junior <charlesjunnr@gmail.com>
2023-03-13 11:54:47 -0300

    Atualizado, exercícios feios.

---
+++ Exercicio027/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Exercicio027
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+           
+            int anterior = 0;
+            int atual = 0;
+            int proximo = 1;
+            string resultado = "0 ";
+            
+            for (int i = 0; i < 20; i++)
+            {
+                anterior = atual;
+                atual = proximo;
+                proximo = atual + anterior;
+                resultado += proximo.ToString() + " ";
+            }
+            System.Console.Write(resultado);
+        }
+    }
+}
+/* Console.Clear();
+            int numero;
+            int anterior = 0;
+            int atual = 1;
+            int proximo;
+
+            Console.Write("Digite um número inteiro: ");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.Write($"\nSequência de Fibonacci até o número {numero}: ");
+
+            while (atual <= numero)
+            {
+                Console.Write($"{atual} ");
+
+                proximo = anterior + atual;
+                anterior = atual;
+                atual = proximo;
+*/
---
+++ Exercicio029/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio029 //tabuada 1 até o 10 (Junior Souza)
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Tabuada do 1 até o 10");
+            Console.WriteLine();
+            int num2 = 10;
+            for (int i = 1; i <= num2; i++)
+            {
+                for (int a = 1; a <= num2; a++) { 
+
+                    int resultado = i * a;
+                    Console.WriteLine(i + " x " + a + " = " + resultado);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
